f23049349a133e89a5efdf20499d4cf27de5e60d
Donato Capitella <donato.capitella@reversec.com>
2026-05-15 07:22:08 +0100

    feat: add MTP support with new Vulkan and ROCm 7.2.3 toolboxes and update base ROCm version to 7.2.3

--- benchmark/run_benchmarks.sh
+++ benchmark/run_benchmarks.sh
@@ -27,7 +27,7 @@ echo
 
 declare -A CMDS=(
   [rocm6_4_4]="toolbox run -c llama-rocm-6.4.4 -- /usr/local/bin/llama-bench"
-  [rocm7_2_2]="toolbox run -c llama-rocm-7.2.2 -- /usr/local/bin/llama-bench"
+  [rocm7_2_3]="toolbox run -c llama-rocm-7.2.3 -- /usr/local/bin/llama-bench"
   [rocm7_nightlies]="toolbox run -c llama-rocm7-nightlies -- /usr/local/bin/llama-bench"
   [vulkan_amdvlk]="toolbox run -c llama-vulkan-amdvlk -- /usr/sbin/llama-bench"
   [vulkan_radv]="toolbox run -c llama-vulkan-radv -- /usr/sbin/llama-bench"
